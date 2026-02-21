f0633ac5f8315a75768acfcbcf8eaba5b023191c
Agne Banzeryte <Agne.banzeryte@gmail.com>
2026-02-21 23:17:11 +0100

    Article model and Articles controller generated

--- Gemfile
+++ Gemfile
@@ -62,3 +62,7 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
+# Faker
+
+gem 'faker'
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+# Faker::Name.name                      #=> "Christophe Bartell"
+# Faker::Internet.password              #=> "Vg5mSvY1UeRg7"
+# Faker::Internet.email                 #=> "eliza@mann.test"
+# Faker::Address.full_address           #=> "5479 William Way, East Sonnyhaven, LA 63637"
+# Faker::Markdown.emphasis              #=> "Quo qui aperiam. Amet corrupti distinctio. Sit quia *dolor.*"
+# Faker::Lorem.paragraph                #=> "Recusandae minima consequatur. Expedita sequi blanditiis. Ut fuga et."
+# Faker::Alphanumeric.alpha(number: 10) #=> "zlvubkrwga"
+# Faker::ProgrammingLanguage.name       #=> "Ruby"
+#
+10.times { article = Article.new(title: Faker::ProgrammingLanguage.name, content: Faker::Lorem.paragraph) }
